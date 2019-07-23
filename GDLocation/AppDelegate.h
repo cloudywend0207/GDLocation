28871251268c2e383b29b745e55cd9b4c8831367
iov_wangyun <v_wangyun-yjs@saicmotor.com>
2019-07-23 14:28:16 +0800

    no message

---
+++ GDLocation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GDLocation
+//
+//  Created by Alexander Wang on 2018/2/5.
+//  Copyright © 2018年 Alexander Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
